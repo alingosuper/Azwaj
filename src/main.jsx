37b1c8c9d64f1bae6237e23a2568063a00de1f02
Alingosuper <Alingosuper@gmail.com>
2026-04-22 19:03:36 +0500

    Emergency fix for white screen - Path correction

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+console.log("System Initializing...");
+
+const root = document.getElementById('root');
+if (root) {
+  ReactDOM.createRoot(root).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
+} else {
+  console.error("Root element not found!");
+}
